a56682f7f87e092c5d5cd58891aa352a7a8c6eaa
Branden J Brown <zephyrtronium@gmail.com>
2020-10-12 14:04:17 -0400

    robot: send slash commands
    An earlier vulnerability in the talk command allowed all users to have the bot send slash commands. Because that command no longer sends a message if it can't continue the chain, and because slash commands are not relayed by Twitch, that vulnerability has been fixed.

--- badmatch.go
+++ badmatch.go
@@ -22,11 +22,6 @@ import (
 )
 
 // badmatch filters PRIVMSG messages that contain bad words.
-//
-// Currently the only bad word is "." at the start of the message.
 func badmatch(msg irc.Message) bool {
-	if len(msg.Trailing) < 2 {
-		return true
-	}
-	return msg.Trailing[0] == '.'
+	return false
 }
