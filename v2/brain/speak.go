3a082842246ec45bab161e1d14cde59546c540bc
Branden J Brown <zephyrtronium@gmail.com>
2022-09-11 16:59:37 -0400

    brain: speak

---
+++ v2/brain/speak.go
@@ -0,0 +1,71 @@
+package brain
+
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
+// Speaker produces random messages.
+type Speaker interface {
+	// Order is the length of prompts given to Speak.
+	Order() int
+	// New finds a prompt to begin a random message. When a message is
+	// generated with no prompt, the result from New is passed directly to
+	// Speak; it is the speaker's responsibility to ensure it meets
+	// requirements with regard to entropy reduction and matchable content.
+	New(ctx context.Context) ([]string, error)
+	// Speak generates a full message from the given prompt. The prompt is
+	// guaranteed to have length equal to the value returned from Order, unless
+	// it is a prompt returned from New. If the number of tokens in the prompt
+	// is smaller than Order, the difference is made up by prepending empty
+	// strings to the prompt. Empty strings at the start and end of the result
+	// will be trimmed.
+	Speak(ctx context.Context, prompt []string) ([]string, error)
+}
+
+// Speak produces a new message from the given prompt.
+func Speak(ctx context.Context, s Speaker, prompt string) (string, error) {
+	toks := Tokens(nil, prompt)
+	if len(toks) == 0 {
+		// No prompt; get one from the speaker instead.
+		var err error
+		toks, err = s.New(ctx)
+		if err != nil {
+			return "", fmt.Errorf("couldn't get a new prompt: %w", err)
+		}
+	} else {
+		// Make sure the prompt is the right size and has empty strings to
+		// make up the difference.
+		n := s.Order()
+		switch {
+		case len(toks) < n:
+			u := make([]string, n-len(toks), n)
+			toks = append(u, toks...)
+		case len(toks) > n:
+			copy(toks, toks[len(toks)-n:])
+			toks = toks[:n]
+		}
+	}
+	r, err := s.Speak(ctx, toks)
+	if err != nil {
+		return "", fmt.Errorf("couldn't speak: %w", err)
+	}
+	return strings.Join(trim(r), " "), nil
+}
+
+// trim removes empty strings from the start and end of r.
+func trim(r []string) []string {
+	for k := len(r) - 1; k >= 0; k-- {
+		if r[k] != "" {
+			r = r[:k+1]
+			break
+		}
+	}
+	for k, v := range r {
+		if v != "" {
+			return r[k:]
+		}
+	}
+	return nil
+}
---
+++ v2/brain/speak_test.go
@@ -0,0 +1,98 @@
+package brain_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/zephyrtronium/robot/v2/brain"
+)
+
+type testSpeaker struct {
+	order  int
+	new    []string
+	prompt []string
+}
+
+func (t *testSpeaker) Order() int {
+	return t.order
+}
+
+func (t *testSpeaker) New(ctx context.Context) ([]string, error) {
+	return t.new, nil
+}
+
+func (t *testSpeaker) Speak(ctx context.Context, prompt []string) ([]string, error) {
+	t.prompt = prompt
+	return prompt, nil
+}
+
+func TestSpeak(t *testing.T) {
+	cases := []struct {
+		name   string
+		prompt string
+		order  int
+		new    []string
+		want   []string
+	}{
+		{
+			name:   "empty",
+			prompt: "",
+			order:  1,
+			new:    nil,
+			want:   nil,
+		},
+		{
+			name:   "empty-new",
+			prompt: "",
+			order:  1,
+			new:    []string{"anime"},
+			want:   []string{"anime"},
+		},
+		{
+			name:   "prompted-short",
+			prompt: "madoka",
+			order:  2,
+			want:   []string{"", "madoka"},
+		},
+		{
+			name:   "prompted-even",
+			prompt: "madoka homura",
+			order:  2,
+			want:   []string{"madoka", "homura"},
+		},
+		{
+			name:   "prompted-long",
+			prompt: "madoka homura anime",
+			order:  2,
+			want:   []string{"homura", "anime"},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			s := testSpeaker{order: c.order, new: c.new}
+			r, err := brain.Speak(context.Background(), &s, c.prompt)
+			if err != nil {
+				t.Error(err)
+			}
+			if diff := cmp.Diff(c.want, s.prompt); diff != "" {
+				t.Errorf("wrong prompt from %q:\n%s", c.prompt, diff)
+			}
+			// Check that each word the speaker gave to Speak appears in
+			// sequence in the result.
+			t.Logf("%q gave result %q", c.prompt, r)
+			for _, w := range c.want {
+				if w == "" {
+					continue
+				}
+				k := strings.Index(r, w)
+				if k < 0 {
+					t.Errorf("%q doesn't appear in %q", w, r)
+					continue
+				}
+				r = r[k:]
+			}
+		})
+	}
+}
