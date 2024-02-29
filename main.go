30cb18fcd5c008343e0165934dac7619812ee5a4
Branden J Brown <zephyrtronium@gmail.com>
2024-02-29 07:38:05 -0600

    robot: refactor cli definition

--- main.go
+++ main.go
@@ -20,110 +20,20 @@ var app = cli.Command{
 	DefaultCommand: "run",
 	Commands: []*cli.Command{
 		{
-			Name:  "init",
-			Usage: "Initialize configured databases",
-			Action: func(ctx context.Context, cmd *cli.Command) error {
-				order := int(cmd.Int("order"))
-				r, err := os.Open(cmd.String("config"))
-				if err != nil {
-					return fmt.Errorf("couldn't open config file: %w", err)
-				}
-				cfg, _, err := Load(ctx, r)
-				if err != nil {
-					return fmt.Errorf("couldn't load config: %w", err)
-				}
-				brain, priv, err := loadDBs(ctx, cfg.DB)
-				if err != nil {
-					return err
-				}
-				if err := sqlbrain.Create(ctx, brain, order); err != nil {
-					return fmt.Errorf("couldn't initialize brain: %w", err)
-				}
-				if err := privacy.Init(ctx, priv); err != nil {
-					return fmt.Errorf("couldn't initialize privacy list: %w", err)
-				}
-				return nil
-			},
+			Name:   "init",
+			Usage:  "Initialize configured databases",
+			Action: cliInit,
 			Flags: []cli.Flag{
-				&cli.StringFlag{
-					Name:     "config",
-					Required: true,
-					Usage:    "TOML config file",
-					Action: func(ctx context.Context, cmd *cli.Command, s string) error {
-						i, err := os.Stat(s)
-						if err != nil {
-							return err
-						}
-						if !i.Mode().IsRegular() {
-							return errors.New("config must be a regular file")
-						}
-						return nil
-					},
-				},
-				&cli.IntFlag{
-					Name:     "order",
-					Required: true,
-					Usage:    "Prefix length for Markov chains",
-					Action: func(ctx context.Context, cmd *cli.Command, i int64) error {
-						if i <= 0 {
-							return errors.New("order must be positive")
-						}
-						return nil
-					},
-				},
+				&flagConfig,
+				&flagOrder,
 			},
 		},
 		{
-			Name:  "run",
-			Usage: "Connect to configured chat services",
-			Action: func(ctx context.Context, cmd *cli.Command) error {
-				r, err := os.Open(cmd.String("config"))
-				if err != nil {
-					return fmt.Errorf("couldn't open config file: %w", err)
-				}
-				cfg, md, err := Load(ctx, r)
-				if err != nil {
-					return fmt.Errorf("couldn't load config: %w", err)
-				}
-				r.Close()
-				robo := New(runtime.GOMAXPROCS(0))
-				robo.SetOwner(cfg.Owner.Name, cfg.Owner.Contact)
-				if err := robo.SetSecrets(cfg.SecretFile); err != nil {
-					return err
-				}
-				brain, priv, err := loadDBs(ctx, cfg.DB)
-				if err != nil {
-					return err
-				}
-				if err := robo.SetSources(ctx, brain, priv); err != nil {
-					return err
-				}
-				if md.IsDefined("tmi") {
-					if err := robo.SetTMI(ctx, cfg.TMI); err != nil {
-						return err
-					}
-					if err := robo.SetTwitchChannels(ctx, cfg.Global, cfg.Twitch); err != nil {
-						return err
-					}
-				}
-				return robo.Run(ctx)
-			},
+			Name:   "run",
+			Usage:  "Connect to configured chat services",
+			Action: cliRun,
 			Flags: []cli.Flag{
-				&cli.StringFlag{
-					Name:     "config",
-					Required: true,
-					Usage:    "TOML config file",
-					Action: func(ctx context.Context, cmd *cli.Command, s string) error {
-						i, err := os.Stat(s)
-						if err != nil {
-							return err
-						}
-						if !i.Mode().IsRegular() {
-							return errors.New("config must be a regular file")
-						}
-						return nil
-					},
-				},
+				&flagConfig,
 			},
 		},
 	},
@@ -131,7 +41,7 @@ var app = cli.Command{
 	Authors: []any{
 		"Branden J Brown  @zephyrtronium",
 	},
-	Copyright: "Copyright 2023 Branden J Brown",
+	Copyright: "Copyright 2024 Branden J Brown",
 }
 
 func main() {
@@ -145,3 +55,89 @@ func main() {
 		fmt.Println(err)
 	}
 }
+
+func cliInit(ctx context.Context, cmd *cli.Command) error {
+	order := int(cmd.Int("order"))
+	r, err := os.Open(cmd.String("config"))
+	if err != nil {
+		return fmt.Errorf("couldn't open config file: %w", err)
+	}
+	cfg, _, err := Load(ctx, r)
+	if err != nil {
+		return fmt.Errorf("couldn't load config: %w", err)
+	}
+	brain, priv, err := loadDBs(ctx, cfg.DB)
+	if err != nil {
+		return err
+	}
+	if err := sqlbrain.Create(ctx, brain, order); err != nil {
+		return fmt.Errorf("couldn't initialize brain: %w", err)
+	}
+	if err := privacy.Init(ctx, priv); err != nil {
+		return fmt.Errorf("couldn't initialize privacy list: %w", err)
+	}
+	return nil
+}
+
+func cliRun(ctx context.Context, cmd *cli.Command) error {
+	r, err := os.Open(cmd.String("config"))
+	if err != nil {
+		return fmt.Errorf("couldn't open config file: %w", err)
+	}
+	cfg, md, err := Load(ctx, r)
+	if err != nil {
+		return fmt.Errorf("couldn't load config: %w", err)
+	}
+	r.Close()
+	robo := New(runtime.GOMAXPROCS(0))
+	robo.SetOwner(cfg.Owner.Name, cfg.Owner.Contact)
+	if err := robo.SetSecrets(cfg.SecretFile); err != nil {
+		return err
+	}
+	brain, priv, err := loadDBs(ctx, cfg.DB)
+	if err != nil {
+		return err
+	}
+	if err := robo.SetSources(ctx, brain, priv); err != nil {
+		return err
+	}
+	if md.IsDefined("tmi") {
+		if err := robo.SetTMI(ctx, cfg.TMI); err != nil {
+			return err
+		}
+		if err := robo.SetTwitchChannels(ctx, cfg.Global, cfg.Twitch); err != nil {
+			return err
+		}
+	}
+	return robo.Run(ctx)
+}
+
+var (
+	flagConfig = cli.StringFlag{
+		Name:     "config",
+		Required: true,
+		Usage:    "TOML config file",
+		Action: func(ctx context.Context, cmd *cli.Command, s string) error {
+			i, err := os.Stat(s)
+			if err != nil {
+				return err
+			}
+			if !i.Mode().IsRegular() {
+				return errors.New("config must be a regular file")
+			}
+			return nil
+		},
+	}
+
+	flagOrder = cli.IntFlag{
+		Name:     "order",
+		Required: true,
+		Usage:    "Prefix length for Markov chains",
+		Action: func(ctx context.Context, cmd *cli.Command, i int64) error {
+			if i <= 0 {
+				return errors.New("order must be positive")
+			}
+			return nil
+		},
+	}
+)
