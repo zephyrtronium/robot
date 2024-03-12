40da4c20ea7ad6d33d2ed1ec134b308cad7920e4
Branden J Brown <zephyrtronium@gmail.com>
2024-03-11 19:14:32 -0500

    brain/braintest: more test cases

--- brain/braintest/braintest.go
+++ brain/braintest/braintest.go
@@ -3,6 +3,8 @@ package braintest
 
 import (
 	"context"
+	"maps"
+	"strings"
 	"testing"
 	"time"
 
@@ -22,44 +24,246 @@ type Interface interface {
 //
 // If a brain cannot be created without error, new should call t.Fatal.
 func Test(ctx context.Context, t *testing.T, new func(context.Context) Interface) {
-	t.Run("forgetful", testForgetful(ctx, new(ctx)))
+	t.Run("speak", testSpeak(ctx, new(ctx)))
+	t.Run("forget", testForget(ctx, new(ctx)))
+	t.Run("forgetMessage", testForgetMessage(ctx, new(ctx)))
+	t.Run("forgetDuring", testForgetDuring(ctx, new(ctx)))
 	t.Run("combinatoric", testCombinatoric(ctx, new(ctx)))
 }
 
-// testForgetful tests that a brain forgets what it forgets.
-func testForgetful(ctx context.Context, br Interface) func(t *testing.T) {
-	return func(t *testing.T) {
-		id := uuid.UUID{1}
-		tag := "bocchi"
-		msg := brain.MessageMeta{
-			ID:   id,
+var messages = [...]struct {
+	brain.MessageMeta
+	Tokens []string
+}{
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{1},
 			User: userhash.Hash{2},
-			Tag:  tag,
+			Tag:  "kessoku",
 			Time: time.Unix(0, 0),
-		}
-		text := "bocchi ryou nijika kita"
-		toks := brain.Tokens(nil, text)
-		if err := brain.Learn(ctx, br, &msg, toks); err != nil {
-			t.Errorf("failed to learn: %v", err)
-		}
-		s, err := brain.Speak(ctx, br, tag, "")
-		if err != nil {
-			t.Errorf("failed to speak: %v", err)
-		}
-		if s != text {
-			t.Errorf("surprise thought: %q", s)
-		}
-		if err := brain.Forget(ctx, br, tag, toks); err != nil {
-			t.Errorf("failed to forget: %v", err)
-		}
-		// We don't really care about an error here, since the brain is empty.
-		// All we care about is no thoughts.
-		s, _ = brain.Speak(ctx, br, tag, "")
-		if s != "" {
-			t.Errorf("remembered that which must be forgotten: %q", s)
+		},
+		Tokens: []string{"member", "bocchi"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{2},
+			User: userhash.Hash{2},
+			Tag:  "kessoku",
+			Time: time.Unix(1, 0),
+		},
+		Tokens: []string{"member", "ryou"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{3},
+			User: userhash.Hash{3},
+			Tag:  "kessoku",
+			Time: time.Unix(2, 0),
+		},
+		Tokens: []string{"member", "nijika"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{4},
+			User: userhash.Hash{3},
+			Tag:  "kessoku",
+			Time: time.Unix(3, 0),
+		},
+		Tokens: []string{"member", "kita"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{5},
+			User: userhash.Hash{2},
+			Tag:  "sickhack",
+			Time: time.Unix(0, 0),
+		},
+		Tokens: []string{"member", "bocchi"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{6},
+			User: userhash.Hash{2},
+			Tag:  "sickhack",
+			Time: time.Unix(1, 0),
+		},
+		Tokens: []string{"member", "ryou"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{7},
+			User: userhash.Hash{3},
+			Tag:  "sickhack",
+			Time: time.Unix(2, 0),
+		},
+		Tokens: []string{"member", "nijika"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{8},
+			User: userhash.Hash{3},
+			Tag:  "sickhack",
+			Time: time.Unix(3, 0),
+		},
+		Tokens: []string{"member", "kita"},
+	},
+	{
+		MessageMeta: brain.MessageMeta{
+			ID:   uuid.UUID{9},
+			User: userhash.Hash{4},
+			Tag:  "sickhack",
+			Time: time.Unix(43, 0),
+		},
+		Tokens: []string{"manager", "seika"},
+	},
+}
+
+func learn(ctx context.Context, t *testing.T, br brain.Learner) {
+	t.Helper()
+	for _, m := range messages {
+		if err := brain.Learn(ctx, br, &m.MessageMeta, m.Tokens); err != nil {
+			t.Fatalf("couldn't learn message %v: %v", m.ID, err)
 		}
 	}
 }
+
+func speak(ctx context.Context, t *testing.T, br brain.Speaker, tag, prompt string, iters int) map[string]bool {
+	t.Helper()
+	got := make(map[string]bool, 5)
+	for range iters {
+		s, err := brain.Speak(ctx, br, tag, prompt)
+		if err != nil {
+			t.Errorf("couldn't speak: %v", err)
+		}
+		got[s] = true
+	}
+	return got
+}
+
+// testSpeak tests that a brain can speak what it has learned.
+func testSpeak(ctx context.Context, br Interface) func(t *testing.T) {
+	return func(t *testing.T) {
+		learn(ctx, t, br)
+		got := speak(ctx, t, br, "kessoku", "", 256)
+		want := map[string]bool{
+			"member bocchi": true,
+			"member ryou":   true,
+			"member nijika": true,
+			"member kita":   true,
+		}
+		if !maps.Equal(got, want) {
+			t.Errorf("wrong spoken messages for kessoku: want %v, got %v", want, got)
+		}
+		got = speak(ctx, t, br, "sickhack", "", 256)
+		want = map[string]bool{
+			"member bocchi": true,
+			"member ryou":   true,
+			"member nijika": true,
+			"member kita":   true,
+			"manager seika": true,
+		}
+		if !maps.Equal(got, want) {
+			t.Errorf("wrong spoken messages for sickhack: want %v, got %v", want, got)
+		}
+		got = speak(ctx, t, br, "sickhack", "manager", 32)
+		want = map[string]bool{
+			"manager seika": true,
+		}
+		if !maps.Equal(got, want) {
+			t.Errorf("wrong prompted messages for sickhack: want %v, got %v", want, got)
+		}
+	}
+}
+
+// testForget tests that a brain forgets what it forgets.
+func testForget(ctx context.Context, br Interface) func(t *testing.T) {
+	return func(t *testing.T) {
+		learn(ctx, t, br)
+		if err := brain.Forget(ctx, br, "kessoku", messages[0].Tokens); err != nil {
+			t.Errorf("couldn't forget: %v", err)
+		}
+		for range 100 {
+			s, err := brain.Speak(ctx, br, "kessoku", "")
+			if err != nil {
+				t.Errorf("couldn't speak: %v", err)
+			}
+			if strings.Contains(s, "bocchi") {
+				t.Errorf("remembered that which must be forgotten: %q", s)
+			}
+		}
+		for {
+			s, err := brain.Speak(ctx, br, "sickhack", "")
+			if err != nil {
+				t.Errorf("couldn't speak: %v", err)
+			}
+			if strings.Contains(s, "bocchi") {
+				break
+			}
+			// The failure condition is that this loop is infinite.
+		}
+	}
+}
+
+// testForgetMessage tests that a brain can forget messages by ID.
+func testForgetMessage(ctx context.Context, br Interface) func(t *testing.T) {
+	return func(t *testing.T) {
+		learn(ctx, t, br)
+		if err := br.ForgetMessage(ctx, "kessoku", messages[0].ID); err != nil {
+			t.Errorf("failed to forget first message: %v", err)
+		}
+		got := speak(ctx, t, br, "kessoku", "", 256)
+		want := map[string]bool{
+			"member ryou":   true,
+			"member nijika": true,
+			"member kita":   true,
+		}
+		if !maps.Equal(got, want) {
+			t.Errorf("wrong messages after forgetting: want %v, got %v", want, got)
+		}
+		got = speak(ctx, t, br, "sickhack", "", 256)
+		want = map[string]bool{
+			"member bocchi": true,
+			"member ryou":   true,
+			"member nijika": true,
+			"member kita":   true,
+			"manager seika": true,
+		}
+		if !maps.Equal(got, want) {
+			t.Errorf("wrong messages in other tag after forgetting: want %v, got %v", want, got)
+		}
+	}
+}
+
+// testForgetDuring tests that a brain can forget messages in a time range.
+func testForgetDuring(ctx context.Context, br Interface) func(t *testing.T) {
+	return func(t *testing.T) {
+		learn(ctx, t, br)
+		if err := br.ForgetDuring(ctx, "kessoku", time.Unix(1, 0).Add(-time.Millisecond), time.Unix(2, 0).Add(time.Millisecond)); err != nil {
+			t.Errorf("failed to forget: %v", err)
+		}
+		got := speak(ctx, t, br, "kessoku", "", 256)
+		want := map[string]bool{
+			"member bocchi": true,
+			"member kita":   true,
+		}
+		if !maps.Equal(got, want) {
+			t.Errorf("wrong messages after forgetting: want %v, got %v", want, got)
+		}
+		got = speak(ctx, t, br, "sickhack", "", 256)
+		want = map[string]bool{
+			"member bocchi": true,
+			"member ryou":   true,
+			"member nijika": true,
+			"member kita":   true,
+			"manager seika": true,
+		}
+		if !maps.Equal(got, want) {
+			t.Errorf("wrong spoken messages for sickhack: want %v, got %v", want, got)
+		}
+	}
+}
+
+// TODO(zeph): testForgetUser
 
 // testCombinatoric tests that chains can generate even with substantial
 // overlap in learned material.
