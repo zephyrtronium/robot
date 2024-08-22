90b0e6af5519356e0e99a9e69313c2e01b4d713a
Branden J Brown <zephyrtronium@gmail.com>
2024-08-22 10:12:02 -0400

    command: consolidate definitions

--- command/command.go
+++ command/command.go
@@ -2,11 +2,24 @@ package command
 
 import (
 	"context"
+	"log/slog"
 
+	"github.com/zephyrtronium/robot/brain"
 	"github.com/zephyrtronium/robot/channel"
 	"github.com/zephyrtronium/robot/message"
+	"github.com/zephyrtronium/robot/privacy"
+	"github.com/zephyrtronium/robot/spoken"
 	"github.com/zephyrtronium/robot/userhash"
 )
+
+// Robot is the bot state as is visible to commands.
+type Robot struct {
+	Log      *slog.Logger
+	Channels map[string]*channel.Channel // TODO(zeph): syncmap[string]channel.Channel
+	Brain    brain.Brain
+	Privacy  *privacy.List
+	Spoken   *spoken.History
+}
 
 // Invocation is a command invocation. An Invocation and its fields must not
 // be modified or retained by any command.
@@ -22,5 +35,5 @@ type Invocation struct {
 	Hasher userhash.Hasher
 }
 
-// Func executes a command.
+// Func is a command function.
 type Func func(ctx context.Context, robo *Robot, call *Invocation)
