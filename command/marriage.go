7311fb43443e29c9d741dde3eec176330e2001e0
Branden J Brown <zephyrtronium@gmail.com>
2024-09-07 18:30:19 -0400

    command: add a variety of affection responses
    Popular command gets the attention while the pending functional
    features suffer...

--- command/marriage.go
+++ command/marriage.go
@@ -10,11 +10,11 @@ import (
 	"time"
 
 	"github.com/zephyrtronium/robot/channel"
+	"gitlab.com/zephyrtronium/pick"
 )
 
-func score(h *channel.History, user string) float64 {
+func score(h *channel.History, user string) (x float64, c, f, l, n int) {
 	mine := make(map[string]map[string]struct{})
-	f, c, l, n := 0, 0, 0, 0
 	for m := range h.All() {
 		who, text := m.Sender, m.Text
 		n++
@@ -47,7 +47,7 @@ func score(h *channel.History, user string) float64 {
 	// There are other criteria we could add, like the number of distinct runes
 	// used in messages you've sent or the uniqueness of your messages based on
 	// a metric like tf-idf. However, this can't be too expensive to compute.
-	x := float64(c*c)/float64(f+1) + float64(c*f+f) + math.Sqrt(float64(l))
+	x = float64(c*c)/float64(f+1) + float64(c*f+f) + math.Sqrt(float64(l))
 	slog.Info("user score",
 		slog.String("user", user),
 		slog.Int("msgs", n),
@@ -56,15 +56,27 @@ func score(h *channel.History, user string) float64 {
 		slog.Int("len", l),
 		slog.Float64("score", x),
 	)
-	return x
+	return x, c, f, l, n
 }
 
 type broadcasterAffectionKey struct{}
 
+var affections = pick.New([]pick.Case[string]{
+	{E: "about %[1]f %[2]s", W: 5},
+	{E: "roughly %[1]f %[2]s", W: 5},
+	{E: "%[1]f or so %[2]s", W: 5},
+	{E: "approximately %[1]f %[2]s", W: 5},
+	{E: "I have calculated my affection for you to be exactly %[1]f %[2]s", W: 1},
+	{E: "Right now, I'd say %[1]f. But who knows what the future may hold? %[2]s", W: 1},
+	{E: "%[1]f, and yes, that is a threat. %[2]s", W: 1},
+	{E: "%.0[1]f %.1[1]f %.2[1]f %.3[1]f %.4[1]f %.5[1]f %.6[1]f %.7[1]f %.8[1]f %.9[1]f", W: 1},
+	{E: "%[1]f, given score = c²/(f+1) + (c+1)f + √l, f=%[4]d from your messages sent, l=%[5]d from length of your longest message, and c=%[3]d from memes, across messages in the last fifteen minutes %[2]s", W: 1},
+})
+
 // Affection describes the caller's affection MMR.
 // No arguments.
 func Affection(ctx context.Context, robo *Robot, call *Invocation) {
-	x := score(call.Channel.History, call.Message.Sender)
+	x, c, f, l, n := score(call.Channel.History, call.Message.Sender)
 	// Anything we do will require an emote.
 	e := call.Channel.Emotes.Pick(rand.Uint32())
 	if x == 0 {
@@ -82,7 +94,8 @@ func Affection(ctx context.Context, robo *Robot, call *Invocation) {
 		call.Channel.Message(ctx, call.Message.ID, "literally zero "+e)
 		return
 	}
-	call.Channel.Message(ctx, call.Message.ID, fmt.Sprintf("about %f %s", x, e))
+	s := affections.Pick(rand.Uint32())
+	call.Channel.Message(ctx, call.Message.ID, fmt.Sprintf(s, x, e, c, f, l, n))
 }
 
 type partnerKey struct{}
@@ -95,7 +108,7 @@ type partner struct {
 // Marry proposes to the robo.
 //   - partnership: Type of partnership requested, e.g. "wife", "waifu", "daddy". Optional.
 func Marry(ctx context.Context, robo *Robot, call *Invocation) {
-	x := score(call.Channel.History, call.Message.Sender)
+	x, _, _, _, _ := score(call.Channel.History, call.Message.Sender)
 	e := call.Channel.Emotes.Pick(rand.Uint32())
 	broadcaster := strings.EqualFold(call.Message.Name, strings.TrimPrefix(call.Channel.Name, "#")) && x == 0
 	if x < 10 && !broadcaster {
@@ -129,7 +142,7 @@ func Marry(ctx context.Context, robo *Robot, call *Invocation) {
 			call.Channel.Message(ctx, call.Message.ID, "My heart yet belongs to another... "+e)
 			return
 		}
-		y := score(call.Channel.History, cur.who)
+		y, _, _, _, _ := score(call.Channel.History, cur.who)
 		if x < y && !broadcaster {
 			call.Channel.Message(ctx, call.Message.ID, "I'm touched, but I must decline. I'm in love with someone else. "+e)
 			return
