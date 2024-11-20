bb10685b8655f8a30c280a9e373ec30002acee7b
Branden J Brown <zephyrtronium@gmail.com>
2024-11-20 16:57:27 -0500

    command: increase power of hte
    hte has triggered a couple dozen times on my instance now but never on
    a message that actually contains a/an/the. Pitiful. Add more typos to
    increase the chance it will do something when activated.

--- command/effect.go
+++ command/effect.go
@@ -2,8 +2,11 @@ package command
 
 import (
 	"log/slog"
+	"math/rand/v2"
 	"strings"
 	"unicode"
+
+	"gitlab.com/zephyrtronium/pick"
 )
 
 // Effect applies an effect to a message.
@@ -77,6 +80,9 @@ func hteize(msg string) string {
 	f := strings.Fields(msg)
 	for i, w := range f {
 		switch w {
+		case "":
+			// Handle this case so we can check for punctuation more easily later.
+			continue
 		case "the", "The":
 			f[i] = "hte"
 		case "THE":
@@ -85,11 +91,94 @@ func hteize(msg string) string {
 			f[i] = "an"
 		case "A":
 			// skip this one
-		case "an", "An":
+		case "an":
 			f[i] = "a"
-		case "AN":
+		case "An", "AN":
 			f[i] = "A"
+		case "your":
+			f[i] = "you're"
+		case "Your":
+			f[i] = "You're"
+		case "YOUR":
+			f[i] = `YOU"RE`
+		case "you're", "you'RE": // tokenization and entropy reduction can lead to you -> ' -> RE
+			f[i] = "your"
+		case "You're", "You'RE":
+			f[i] = "Your"
+		case "YOU'RE", "YOU're", `YOU"RE`:
+			f[i] = "YORU" // rare treat
+		case "there":
+			f[i] = thereLower.Pick(rand.Uint32())
+		case "There":
+			f[i] = thereTitle.Pick(rand.Uint32())
+		case "THERE":
+			f[i] = thereUpper.Pick(rand.Uint32())
+		case "their":
+			f[i] = theirLower.Pick(rand.Uint32())
+		case "Their":
+			f[i] = theirTitle.Pick(rand.Uint32())
+		case "THEIR":
+			f[i] = theirUpper.Pick(rand.Uint32())
+		case "they're":
+			f[i] = theyreLower.Pick(rand.Uint32())
+		case "They're":
+			f[i] = theyreTitle.Pick(rand.Uint32())
+		case "THEY'RE", `THEY"RE`:
+			f[i] = theyreUpper.Pick(rand.Uint32())
+		case "its":
+			f[i] = "it's"
+		case "Its":
+			f[i] = "It's"
+		case "ITS":
+			f[i] = `IT"S`
+		case "it's":
+			f[i] = "its"
+		case "It's":
+			f[i] = "Its"
+		case "IT'S", `IT"S`:
+			f[i] = "ITS"
+		case "then":
+			f[i] = "than"
+		case "Then":
+			f[i] = "Than"
+		case "THEN":
+			f[i] = "THAN"
+		case "than":
+			f[i] = "then"
+		case "Than":
+			f[i] = "Then"
+		case "THAN":
+			f[i] = "THEN"
+		case "should've", "should'VE":
+			f[i] = "should of"
+		case "Should've", "Should'VE":
+			f[i] = "Should of"
+		case "SHOULD'VE", "SHOULD've", `SHOULD"VE`:
+			f[i] = "SHOULD OF"
+		case "it", "It":
+			f[i] = "ti"
+		case "IT":
+			f[i] = "TI"
+		default:
+			k := strings.IndexAny(w, ".?!")
+			// Note we check for k > 1, not k >= 0, because we don't want to
+			// do this when the character is at the start.
+			if k > 1 {
+				f[i] = f[i][:k] + " " + f[i][k:]
+			}
 		}
 	}
 	return strings.Join(f, " ")
 }
+
+var (
+	thereLower  = pick.New(pick.FromMap(map[string]int{"their": 2, "they're": 2, "three": 1}))
+	thereTitle  = pick.New(pick.FromMap(map[string]int{"Their": 2, "They're": 2, "Three": 1}))
+	thereUpper  = pick.New(pick.FromMap(map[string]int{"THEIR": 4, `THEY"RE`: 2, `THEYRE`: 2, "THREE": 1}))
+	theirLower  = pick.New(pick.FromMap(map[string]int{"there": 2, "they're": 2}))
+	theirTitle  = pick.New(pick.FromMap(map[string]int{"There": 2, "They're": 2}))
+	theirUpper  = pick.New(pick.FromMap(map[string]int{"THERE": 4, `THEY"RE`: 2, `THEYRE`: 2}))
+	theyreLower = pick.New(pick.FromMap(map[string]int{"there": 2, "their": 2}))
+	theyreTitle = pick.New(pick.FromMap(map[string]int{"There": 2, "Their": 2}))
+	theyreUpper = pick.New(pick.FromMap(map[string]int{"THERE": 4, "THEIR": 4}))
+)
