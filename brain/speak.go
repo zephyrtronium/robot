14465d5a52c81cd4eefc88189d4fdf902f664be1
Branden J Brown <zephyrtronium@gmail.com>
2024-12-19 11:18:05 -0500

    brain: implement speaking using new method
    For #94.

--- brain/speak.go
+++ brain/speak.go
@@ -1,17 +1,22 @@
 package brain
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"math/rand/v2"
 	"slices"
 	"strings"
 
+	"github.com/zephyrtronium/robot/deque"
 	"github.com/zephyrtronium/robot/tpool"
 )
 
 var (
-	tokensPool  tpool.Pool[[]string]
-	builderPool = tpool.Pool[*Builder]{New: func() any { return new(Builder) }}
+	tokensPool    tpool.Pool[[]string]
+	prependerPool tpool.Pool[deque.Deque[string]]
+	bytesPool     tpool.Pool[[]byte]
+	builderPool   = tpool.Pool[*Builder]{New: func() any { return new(Builder) }}
 )
 
 // Speak produces a new message and the trace of messages used to form it
@@ -40,4 +45,103 @@ func Speak(ctx context.Context, s Interface, tag, prompt string) (string, []stri
 		return "", nil, nil
 	}
 	return strings.TrimSpace(w.String()), slices.Clone(w.Trace()), nil
+}
+
+// Think produces a new message and the trace of message IDs used to form it
+// from the given prompt.
+// If the brain does not produce any terms, the result is the empty string
+// regardless of the prompt, with no error.
+func Think(ctx context.Context, s Interface, tag, prompt string) (string, []string, error) {
+	w := bytesPool.Get()
+	toks := tokens(tokensPool.Get(), prompt)
+	for i, t := range toks {
+		w = append(w, t...)
+		toks[i] = ReduceEntropy(t)
+	}
+	slices.Reverse(toks)
+	search := prependerPool.Get().Prepend(toks...)
+	defer func() {
+		bytesPool.Put(w[:0])
+		tokensPool.Put(toks[:0])
+		prependerPool.Put(search.Reset())
+	}()
+
+	var ids []string
+	// We handle the first search specially.
+	// TODO(zeph): skip terminating choices
+	id, tok, l, err := next(ctx, s, tag, search.Slice())
+	if len(tok) == 0 {
+		return "", nil, err
+	}
+	k, ok := slices.BinarySearch(ids, id)
+	if !ok {
+		ids = slices.Insert(ids, k, id)
+	}
+	w = append(w, tok...)
+	search = search.DropEnd(search.Len() - l - 1).Prepend(ReduceEntropy(tok))
+	for range 1024 {
+		id, tok, l, err := next(ctx, s, tag, search.Slice())
+		if len(tok) == 0 {
+			// This could mean the message is done, there was no match for
+			// the prefix, or an error occurred.
+			return string(bytes.TrimSpace(w)), ids, err
+		}
+		k, ok := slices.BinarySearch(ids, id)
+		if !ok {
+			ids = slices.Insert(ids, k, id)
+		}
+		w = append(w, tok...)
+		search = search.DropEnd(search.Len() - l - 1).Prepend(ReduceEntropy(tok))
+	}
+	return string(bytes.TrimSpace(w)), ids, nil
+}
+
+// next finds a single next term from a brain given a prompt.
+func next(ctx context.Context, s Interface, tag string, prompt []string) (id, tok string, l int, err error) {
+	wid := make([]byte, 0, 64)
+	wtok := make([]byte, 0, 64)
+	var skip Skip
+	var n uint64
+	for {
+		var seen uint64
+		n, seen, err = term(ctx, s, tag, prompt, &wid, &wtok, &skip, n)
+		if err != nil {
+			return "", "", 0, err
+		}
+		// Try to lose context.
+		// We want to do so when we have context but see zero options at all,
+		// we have a long context and almost no options,
+		// or at random with even a short context.
+		//
+		// Note that in the third case we use a 1/2 chance; it seems high, but
+		// n.b. the caller will recover the last token that we discard.
+		// This logic is also what makes the first case necessary: we can
+		// randomly drop context, find a result that is impossible otherwise,
+		// and then suddenly halt generation mid-sentence on the next token
+		// because we don't shorten back to where we were.
+		if len(prompt) > 1 && seen == 0 || len(prompt) > 4 && seen <= 2 || len(prompt) > 2 && rand.Uint32()&1 == 0 {
+			prompt = prompt[:len(prompt)-1]
+			continue
+		}
+		// Note that this also handles the case where there were no results.
+		return string(wid), string(wtok), len(prompt), nil
+	}
+}
+
+// term gets the thought for a single prompt and skip sequence with a starting
+// skip length, returning the new skip length and the total number skipped.
+func term(ctx context.Context, s Interface, tag string, prompt []string, wid, wtok *[]byte, skip *Skip, n uint64) (uint64, uint64, error) {
+	var seen uint64
+	for f := range s.Think(ctx, tag, prompt) {
+		seen++
+		if n > 0 {
+			n--
+			continue
+		}
+		if err := f(wid, wtok); err != nil {
+			return n, seen, fmt.Errorf("couldn't think: %w", err)
+		}
+		n = skip.N(rand.Uint64(), rand.Uint64())
+	}
+	return n, seen, nil
 }
--- brain/speak_test.go
+++ brain/speak_test.go
@@ -3,54 +3,76 @@ package brain_test
 import (
 	"context"
 	"iter"
+	"slices"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 
 	"github.com/zephyrtronium/robot/brain"
 	"github.com/zephyrtronium/robot/message"
 	"github.com/zephyrtronium/robot/userhash"
 )
 
-type testSpeaker struct {
-	prompt []string
-	id     string
-	append []byte
+func hasPrefix[S ~[]E, E comparable](long, short S) bool {
+	if len(short) > len(long) {
+		return false
+	}
+	return slices.Equal(long[:len(short)], short)
 }
 
-func (t *testSpeaker) Think(ctx context.Context, tag string, prefix []string) iter.Seq[func(id *[]byte, suf *[]byte) error] {
-	panic("unimplemented")
+type testThinker struct {
+	pres [][]string
+	id   string
+	tups []brain.Tuple
 }
 
-func (t *testSpeaker) Speak(ctx context.Context, tag string, prompt []string, w *brain.Builder) error {
-	t.prompt = prompt
-	w.Append(t.id, t.append)
-	return nil
+func (t *testThinker) Think(ctx context.Context, tag string, prefix []string) iter.Seq[func(id *[]byte, suf *[]byte) error] {
+	t.pres = append(t.pres, slices.Clone(prefix))
+	return func(yield func(func(id *[]byte, suf *[]byte) error) bool) {
+		var w string
+		f := func(id, suf *[]byte) error {
+			*id = []byte(t.id)
+			*suf = []byte(w)
+			return nil
+		}
+		for _, v := range t.tups {
+			if !hasPrefix(v.Prefix, prefix) {
+				continue
+			}
+			w = v.Suffix
+			if !yield(f) {
+				break
+			}
+		}
+	}
+}
+
+func (t *testThinker) Speak(ctx context.Context, tag string, prompt []string, w *brain.Builder) error {
+	panic("TODO: remove")
 }
 
 // Forget implements brain.Brain.
-func (t *testSpeaker) Forget(ctx context.Context, tag string, id string) error {
+func (t *testThinker) Forget(ctx context.Context, tag string, id string) error {
 	panic("unimplemented")
 }
 
 // Learn implements brain.Brain.
-func (t *testSpeaker) Learn(ctx context.Context, tag string, msg *message.Received[userhash.Hash], tuples []brain.Tuple) error {
+func (t *testThinker) Learn(ctx context.Context, tag string, msg *message.Received[userhash.Hash], tuples []brain.Tuple) error {
 	panic("unimplemented")
 }
 
 // Recall implements brain.Brain.
-func (t *testSpeaker) Recall(ctx context.Context, tag string, page string, out []message.Received[userhash.Hash]) (n int, next string, err error) {
+func (t *testThinker) Recall(ctx context.Context, tag string, page string, out []message.Received[userhash.Hash]) (n int, next string, err error) {
 	panic("unimplemented")
 }
 
-func TestSpeak(t *testing.T) {
+func TestThink(t *testing.T) {
 	cases := []struct {
 		name   string
 		prompt string
 		id     string
-		append []byte
-		want   []string
+		tups   []brain.Tuple
+		pres   [][]string
 		trace  []string
 		say    string
 	}{
@@ -58,57 +80,74 @@ func TestSpeak(t *testing.T) {
 			name:   "empty",
 			prompt: "",
 			id:     "kessoku",
-			append: nil,
-			want:   nil,
-			trace:  []string{"kessoku"},
+			tups:   nil,
+			pres:   [][]string{{}},
+			trace:  nil,
 			say:    "",
 		},
 		{
 			name:   "empty-add",
 			prompt: "",
 			id:     "kessoku",
-			append: []byte("bocchi"),
-			want:   nil,
-			trace:  []string{"kessoku"},
-			say:    "bocchi",
+			tups: []brain.Tuple{
+				{
+					Prefix: nil,
+					Suffix: "bocchi",
+				},
+			},
+			pres:  [][]string{{}, {"bocchi"}},
+			trace: []string{"kessoku"},
+			say:   "bocchi",
 		},
 		{
-			name:   "prompted",
+			name:   "prompted-empty",
 			prompt: "bocchi ryo nijika",
 			id:     "kessoku",
-			append: nil,
-			want:   []string{"nijika ", "ryo ", "bocchi "},
-			trace:  []string{"kessoku"},
-			say:    "bocchi ryo nijika",
+			tups:   nil,
+			pres:   [][]string{{"nijika ", "ryo ", "bocchi "}},
+			trace:  nil,
+			say:    "",
 		},
 		{
 			name:   "prompted-add",
 			prompt: "bocchi ryo nijika",
 			id:     "kessoku",
-			append: []byte("kita"),
-			want:   []string{"nijika ", "ryo ", "bocchi "},
-			trace:  []string{"kessoku"},
-			say:    "bocchi ryo nijika kita",
+			tups: []brain.Tuple{
+				{
+					Prefix: []string{"nijika "},
+					Suffix: "kita",
+				},
+			},
+			pres:  [][]string{{"nijika ", "ryo ", "bocchi "}},
+			trace: []string{"kessoku"},
+			say:   "bocchi ryo nijika kita",
 		},
 		{
 			name:   "entropy",
 			prompt: "BOCCHI RYO NIJIKA",
 			id:     "kessoku",
-			append: []byte("KITA"),
-			want:   []string{"nijika ", "ryo ", "bocchi "},
-			trace:  []string{"kessoku"},
-			say:    "BOCCHI RYO NIJIKA KITA",
+			tups: []brain.Tuple{
+				{
+					Prefix: []string{"nijika "},
+					Suffix: "KITA",
+				},
+			},
+			pres:  [][]string{{"nijika ", "ryo ", "bocchi "}},
+			trace: []string{"kessoku"},
+			say:   "BOCCHI RYO NIJIKA KITA",
 		},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			s := testSpeaker{id: c.id, append: c.append}
-			r, trace, err := brain.Speak(context.Background(), &s, "", c.prompt)
+			s := testThinker{id: c.id, tups: c.tups}
+			r, trace, err := brain.Think(context.Background(), &s, "", c.prompt)
 			if err != nil {
 				t.Error(err)
 			}
-			if diff := cmp.Diff(c.want, s.prompt, cmpopts.EquateEmpty()); diff != "" {
-				t.Errorf("wrong prompt from %q:\n%s", c.prompt, diff)
+			for _, want := range c.pres {
+				if !slices.ContainsFunc(s.pres, func(got []string) bool { return slices.Equal(got, want) }) {
+					t.Errorf("missing prompt %q in %q", want, s.pres)
+				}
 			}
 			if diff := cmp.Diff(c.say, r); diff != "" {
 				t.Errorf("wrong result from %q:\n%s", c.say, diff)
