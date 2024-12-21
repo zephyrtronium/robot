a35e7cbe1d5b798e14ebc01f166335b0f86c2322
Branden J Brown <zephyrtronium@gmail.com>
2024-12-21 10:40:21 -0500

    brain/kvbrain: implement think
    For #94.

--- brain/kvbrain/speak.go
+++ brain/kvbrain/speak.go
@@ -16,8 +16,68 @@ import (
 
 var prependerPool tpool.Pool[deque.Deque[string]]
 
-func (m *Brain) Think(ctx context.Context, tag string, prefix []string) iter.Seq[func(id *[]byte, suf *[]byte) error] {
-	panic("unimplemented")
+func (br *Brain) Think(ctx context.Context, tag string, prompt []string) iter.Seq[func(id *[]byte, suf *[]byte) error] {
+	return func(yield func(func(id *[]byte, suf *[]byte) error) bool) {
+		erf := func(err error) { yield(func(id, suf *[]byte) error { return err }) }
+		opts := badger.DefaultIteratorOptions
+		opts.Prefix = hashTag(make([]byte, 0, tagHashLen), tag)
+		// We don't actually need to iterate over values, only the single value
+		// that we decide to use per suffix. So, we can disable value prefetch.
+		opts.PrefetchValues = false
+		b := make([]byte, 0, 128)
+		b = append(b, opts.Prefix...)
+		b = appendPrefix(b, prompt)
+		if len(prompt) == 0 {
+			// If we have no prompt, then we want to make sure we select only
+			// options that start a message.
+			// TODO(zeph): a better way to do this would be to use \x00 as the
+			// terminator so that we can append this unconditionally and then
+			// we always seek to the start of the iteration.
+			b = append(b, '\xff')
+		}
+		var key []byte
+		var item *badger.Item
+		err := br.knowledge.View(func(txn *badger.Txn) error {
+			it := txn.NewIterator(opts)
+			defer it.Close()
+			it.Seek(b)
+			// NOTE(zeph): We're doing some cursed things to limit allocations.
+			// We yield this same closure on every iteration of the loop, but
+			// it uses variables key and item that aren't actually set until
+			// after its definition, because our half of the loop needs to use
+			// them too.
+			f := func(id, suf *[]byte) error {
+				// The id is everything after the first byte following the hash
+				// for empty prefixes, and everything after the first \xff\xff
+				// otherwise.
+				k := key[tagHashLen+1:]
+				if len(prompt) > 0 {
+					_, k, _ = bytes.Cut(k, []byte{0xff, 0xff})
+				}
+				*id = k
+				var err error
+				*suf, err = item.ValueCopy(*suf)
+				if err != nil {
+					return fmt.Errorf("couldn't get item for key %q: %w", key, err)
+				}
+				return nil
+			}
+			for it.ValidForPrefix(b) {
+				item = it.Item()
+				// TODO(zeph): for #43, check deleted uuids so we never pick
+				// a message that has been deleted
+				key = item.KeyCopy(key[:0])
+				if !yield(f) {
+					break
+				}
+				it.Next()
+			}
+			return nil
+		})
+		if err != nil {
+			erf(fmt.Errorf("couldn't read knowledge: %w", err))
+		}
+	}
 }
 
 // Speak generates a full message and appends it to w.
--- brain/kvbrain/speak_test.go
+++ brain/kvbrain/speak_test.go
@@ -14,6 +14,166 @@ import (
 	"github.com/zephyrtronium/robot/brain/braintest"
 )
 
+func TestThink(t *testing.T) {
+	cases := []struct {
+		name   string
+		kvs    [][2]string
+		tag    string
+		prompt []string
+		want   []string
+	}{
+		{
+			name:   "empty",
+			kvs:    nil,
+			tag:    "kessoku",
+			prompt: nil,
+			want:   nil,
+		},
+		{
+			name: "empty-tagged",
+			kvs: [][2]string{
+				{mkey("kessoku", "\xff", "1"), "bocchi "},
+				{mkey("kessoku", "bocchi \xff\xff", "1"), ""},
+			},
+			tag:    "sickhack",
+			prompt: nil,
+			want:   nil,
+		},
+		{
+			name: "empty-prompted",
+			kvs: [][2]string{
+				{mkey("kessoku", "\xff", "1"), "bocchi "},
+				{mkey("kessoku", "bocchi \xff\xff", "1"), ""},
+			},
+			tag:    "kessoku",
+			prompt: []string{"kikuri "},
+			want:   nil,
+		},
+		{
+			name: "single",
+			kvs: [][2]string{
+				{mkey("kessoku", "\xff", "1"), "bocchi "},
+				{mkey("kessoku", "bocchi \xff\xff", "1"), ""},
+			},
+			tag:    "kessoku",
+			prompt: nil,
+			want:   []string{"bocchi "},
+		},
+		{
+			name: "several",
+			kvs: [][2]string{
+				{mkey("kessoku", "\xff", "1"), "bocchi "},
+				{mkey("kessoku", "bocchi \xff\xff", "1"), "ryo "},
+				{mkey("kessoku", "ryo \xffbocchi \xff\xff", "1"), "nijika "},
+				{mkey("kessoku", "kita \xffryo \xffbocchi \xff\xff", "1"), ""},
+			},
+			tag:    "kessoku",
+			prompt: nil,
+			want:   []string{"bocchi "},
+		},
+		{
+			name: "multi",
+			kvs: [][2]string{
+				{mkey("kessoku", "\xff", "1"), "member "},
+				{mkey("kessoku", "member \xff\xff", "1"), "bocchi "},
+				{mkey("kessoku", "bocchi \xffmember \xff\xff", "1"), ""},
+				{mkey("kessoku", "\xff", "2"), "member "},
+				{mkey("kessoku", "member \xff\xff", "2"), "ryo "},
+				{mkey("kessoku", "ryo \xffmember \xff\xff", "2"), ""},
+				{mkey("kessoku", "\xff", "3"), "member "},
+				{mkey("kessoku", "member \xff\xff", "3"), "nijika "},
+				{mkey("kessoku", "nijika \xffmember \xff\xff", "3"), ""},
+				{mkey("kessoku", "\xff", "4"), "member "},
+				{mkey("kessoku", "member \xff\xff", "4"), "kita "},
+				{mkey("kessoku", "kita \xffmember \xff\xff", "4"), ""},
+			},
+			tag:    "kessoku",
+			prompt: nil,
+			want:   []string{"member ", "member ", "member ", "member "},
+		},
+		{
+			name: "multi-prompted",
+			kvs: [][2]string{
+				{mkey("kessoku", "\xff", "1"), "member "},
+				{mkey("kessoku", "member \xff\xff", "1"), "bocchi "},
+				{mkey("kessoku", "bocchi \xffmember \xff\xff", "1"), ""},
+				{mkey("kessoku", "\xff", "2"), "member "},
+				{mkey("kessoku", "member \xff\xff", "2"), "ryo "},
+				{mkey("kessoku", "ryo \xffmember \xff\xff", "2"), ""},
+				{mkey("kessoku", "\xff", "3"), "member "},
+				{mkey("kessoku", "member \xff\xff", "3"), "nijika "},
+				{mkey("kessoku", "nijika \xffmember \xff\xff", "3"), ""},
+				{mkey("kessoku", "\xff", "4"), "member "},
+				{mkey("kessoku", "member \xff\xff", "4"), "kita "},
+				{mkey("kessoku", "kita \xffmember \xff\xff", "4"), ""},
+			},
+			tag:    "kessoku",
+			prompt: []string{"member "},
+			want:   []string{"bocchi ", "ryo ", "nijika ", "kita "},
+		},
+		{
+			name: "multi-tagged",
+			kvs: [][2]string{
+				{mkey("kessoku", "\xff", "1"), "member "},
+				{mkey("kessoku", "member \xff\xff", "1"), "bocchi "},
+				{mkey("kessoku", "bocchi \xffmember \xff\xff", "1"), ""},
+				{mkey("kessoku", "\xff", "2"), "member "},
+				{mkey("kessoku", "member \xff\xff", "2"), "ryo "},
+				{mkey("kessoku", "ryo \xffmember \xff\xff", "2"), ""},
+				{mkey("kessoku", "\xff", "3"), "member "},
+				{mkey("kessoku", "member \xff\xff", "3"), "nijika "},
+				{mkey("kessoku", "nijika \xffmember \xff\xff", "3"), ""},
+				{mkey("kessoku", "\xff", "4"), "member "},
+				{mkey("kessoku", "member \xff\xff", "4"), "kita "},
+				{mkey("kessoku", "kita \xffmember \xff\xff", "4"), ""},
+				{mkey("sickhack", "\xff", "5"), "member "},
+				{mkey("sickhack", "member \xff\xff", "5"), "kikuri "},
+				{mkey("sickhack", "kikuri \xffmember \xff\xff", "5"), ""},
+				{mkey("sickhack", "\xff", "6"), "member "},
+				{mkey("sickhack", "member \xff\xff", "6"), "eliza "},
+				{mkey("sickhack", "eliza \xffmember \xff\xff", "6"), ""},
+				{mkey("sickhack", "\xff", "7"), "member "},
+				{mkey("sickhack", "member \xff\xff", "7"), "shima "},
+				{mkey("sickhack", "shima \xffmember \xff\xff", "7"), ""},
+			},
+			tag:    "sickhack",
+			prompt: nil,
+			want:   []string{"member ", "member ", "member "},
+		},
+		// TODO(zeph): forgotten terms; these aren't implemented correctly yet
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true).WithLogger(nil))
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = db.Update(func(txn *badger.Txn) error {
+				var err error
+				for _, item := range c.kvs {
+					err = errors.Join(err, txn.Set([]byte(item[0]), []byte(item[1])))
+				}
+				return err
+			})
+			if err != nil {
+				t.Errorf("couldn't set up knowledge: %v", err)
+			}
+			br := New(db)
+			_, got, err := braintest.Collect(br.Think(ctx, c.tag, c.prompt))
+			if err != nil {
+				t.Errorf("couldn't think: %v", err)
+			}
+			slices.Sort(c.want)
+			slices.Sort(got)
+			if !slices.Equal(c.want, got) {
+				t.Errorf("wrong results:\nwant %q\ngot  %q", c.want, got)
+			}
+		})
+	}
+}
+
 func TestSpeak(t *testing.T) {
 	uu := ":)"
 	cases := []struct {
